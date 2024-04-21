0043b23eceb04c771f267d7f5507852a5659ef5c
Srinath Avadhanula <srinathava@gmail.com>
2024-04-21 00:08:52 -0400

    add copilot chat

--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -171,6 +171,19 @@ return {
             require("copilot_cmp").setup()
         end,
     },
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        branch = "canary",
+        dependencies = {
+            { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+            { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+        },
+        opts = {
+            debug = true, -- Enable debugging
+            -- See Configuration section for rest
+        },
+        cmd = { 'CopilotChat' }
+    },
 
 
 }
